1cbf1336f803468154afaf4e90a646f3467b6252
Angeleena05 <angeleena.singh@dowjones.com>
2024-07-02 00:22:23 -0400

    SG_tf

--- SG/main.tf
+++ SG/main.tf
@@ -2,6 +2,53 @@ provider "aws" {
   region = "us-east-1" 
 }
 
-resource "aws_security_group" "Terraform_sg" {
-  
+resource "aws_security_group" "security_group_test" {
+  name        = "security_group_test"
+  description = "SG managed by Terraform"
+  vpc_id      = "vpc-0d822cffdb28fac51"  
+
+  ingress {
+    description = ""
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = ""
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = ""
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "RDP access"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = ""
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "security_group_test"
+  }
 }
+
